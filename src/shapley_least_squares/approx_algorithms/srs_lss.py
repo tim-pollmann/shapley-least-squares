5f64264870eb026a3cb8d5adbe4e2917b0d9f960
Tim Pollmann <84510339+tim-pollmann@users.noreply.github.com>
2026-03-23 15:17:34 +0100

    Add first draft for SRSLSS warmup phase

--- src/shapley_least_squares/approx_algorithms/srs_lss.py
+++ src/shapley_least_squares/approx_algorithms/srs_lss.py
@@ -23,40 +23,35 @@ class SRSLSS(ApproxAlgorithmInterface):
     @staticmethod
     @override
     def run(game: GameInterface, T: int) -> np.ndarray:
+        a_si_map, n_samples_used = SRSLSS._warmup(game, T)
+        assert n_samples_used < T
+
         n = game.n
         N = np.array(range(n))
         v_N = game.v(N)
-        a_i_map = {i: 0.0 for i in N}
+        a_i_arr = np.zeros(n)
 
         tau_s = int(np.ceil(T / (n - 1)))
         n_samples_used = 0
+
         for s in range(1, n):
-            tau_map = {i: 0 for i in N}
-            a_is_map = {i: 0.0 for i in N}
+            tau_arr = np.ones(n)
+            a_is_arr = a_si_map[s]
 
             for _ in range(tau_s):
                 S = random.sample(N.tolist(), s)
                 v_S = game.v(S)
                 n_samples_used += 1
-                for i in N:
-                    if i in S:
-                        a_is_map[i] += v_S
-                        tau_map[i] += 1
+
+                for i in S:
+                    a_is_arr[i] += v_S
+                    tau_arr[i] += 1
 
             for i in N:
-                if tau_map[i] == 0:
-                    return np.full(n, np.nan)
+                a_i_arr[i] += a_is_arr[i] / (tau_arr[i] * (n - s))
 
-                a_i_map[i] += a_is_map[i] / (tau_map[i] * (n - s))
-
-        shapley_values = np.array(
-            [
-                v_N / n
-                + ((n - 1) / n) * a_i_map[i]
-                - (1 / n) * sum(a_i_map[j] for j in N if j != i)
-                for i in N
-            ]
-        )
+        a_i_sum = np.sum(a_i_arr)
+        shapley_values = a_i_arr + (v_N - a_i_sum) / n
 
         check_number_of_samples_used(
             n_samples_used,
@@ -64,7 +59,37 @@ class SRSLSS(ApproxAlgorithmInterface):
             SRSLSS.name(),
             max_deviation=n - 1,
         )
+
         return shapley_values
+
+    def _warmup(game: GameInterface, T: int) -> tuple[dict[int, np.ndarray], int]:
+        n = game.n
+        N = np.array(range(n))
+
+        a_si_map = {s: np.zeros(n) for s in range(1, n)}
+
+        n_samples_used = 0
+
+        for s in range(1, n):
+            rand_perm = np.random.permutation(N)
+            K = np.ceil(n / s).astype(int)
+
+            for k in range(K):
+                if n % s == 0 or k < K - 1:
+                    S = rand_perm[k * s : (k + 1) * s]
+                    v_S = game.v(S)
+                else:
+                    S = rand_perm[k * s : n]
+                    S_filler = np.random.choice(
+                        rand_perm[: k * s], s - (n % s), replace=False
+                    )
+                    v_S = game.v(np.concatenate((S, S_filler)))
+
+                n_samples_used += 1
+                for i in S:
+                    a_si_map[s][i] += v_S
+
+        return a_si_map, n_samples_used
 
     @staticmethod
     @override
